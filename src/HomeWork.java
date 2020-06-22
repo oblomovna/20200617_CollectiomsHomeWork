b8c0287afc14f54179789e29884921e5e2dddd51
oblomovna <marina-tkachenko-91@bk.ru>
2020-06-22 18:58:11 +0200

    +one exercise

--- src/HomeWork.java
+++ src/HomeWork.java
@@ -12,7 +12,11 @@ public class HomeWork {
         list2.add(2);
         list2.add(3);
         list2.add(8);
+
+
         List<String> names = Arrays.asList("Ivan", "Maria", "Stephan", "John", "Amalia");
+        System.out.println("getListWithoutNames");
+        System.out.println(getListWithoutNames(names));
         System.out.println("comparesElements3");
         System.out.println(comparesElements3(list1, list2));
         List<Integer> listNum1 = new ArrayList();
@@ -26,12 +30,16 @@ public class HomeWork {
         System.out.println("getListWithNumbersLowerThenNum7");
         System.out.println(getListWithNumbersLowerThenNum7(listNum1, 3));
     }
-   /* public static List<String> getListWithoutNames(List<Integer> list) {
 
-
-
-
-    }*/
+   public static List<String> getListWithoutNames(List<String>list) {
+       List<String> names = new ArrayList();
+       for (int i = 0; i < list.size(); i++) {
+           if (list.get(i).length() != 4) {
+              names.add(list.get(i));
+           }
+       }
+       return names;
+   }
 
     public static List<String> comparesElements3(List<Integer> list1, List<Integer> list2) {
         List<String> comparisonResult = new ArrayList();
@@ -64,14 +72,12 @@ public class HomeWork {
 
     public static List<Integer> getListWithNumbersLowerThenNum7(List<Integer> list, int num) {
         List<Integer> numbersLowerThenNum = new ArrayList();
-
         for(int i = 0; i < list.size(); ++i) {
             if ((Integer)list.get(i) < num) {
                 numbersLowerThenNum.add(list.get(i));
                 Collections.reverse(numbersLowerThenNum);
             }
         }
-
         return numbersLowerThenNum;
     }
 }
